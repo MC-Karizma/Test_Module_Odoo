07eea9b4b097ae27f7ed414f3f0d26715607a507
MC-Karizma <c.mamady@karizma.ma>
2022-12-30 15:44:00 +0100

    [ADD] Ajout droit d'accès, bouton sur la vue liste

--- addons/kzm_instance_request/models/kzm_instance_request.py
+++ addons/kzm_instance_request/models/kzm_instance_request.py
@@ -8,32 +8,37 @@ class KzmInstanceRequest(models.Model):
     _name = 'kzm.instance.request'
     _description = 'Request for Proceedings'
 
-    name = fields.Char("Designation")
-    address_ip = fields.Char("IP Address")
-    active = fields.Boolean()
-    cpu = fields.Char("CPU")
-    ram = fields.Char("RAM")
-    disk = fields.Char("DISK")
-    url = fields.Char("URL")
-    state = fields.Selection([('D', 'Draft'), ('S', 'Submitted'), ('I_P', 'In process'), ('P', 'Processed')],
-                             default='B')
-    limit_date = fields.Date()
-    treat_date = fields.Datetime()
-    treat_duration = fields.Float()
+    name = fields.Char(string="Designation")
+    address_ip = fields.Char(string="IP Address")
+    active = fields.Boolean(string="Active", default=True)
+    cpu = fields.Char(string="CPU")
+    ram = fields.Char(string="RAM")
+    disk = fields.Char(string="DISK")
+    url = fields.Char(string="URL")
+    state = fields.Selection(
+        [('Draft', 'Draft'), ('Submitted', 'Submitted'), ('In process', 'In process'), ('Processed', 'Processed')],
+        default='Draft', string="State")
+    limit_date = fields.Date(string="Limit date")
+    treat_date = fields.Datetime(string="Treat date")
+    treat_duration = fields.Float(string="Treat duration")
 
     def action_draft(self):
-        self.state = "D"
+        for x in self:
+            x.state = "Draft"
 
     def action_submitted(self):
-        self.state = "S"
+        for x in self:
+            x.state = "Submitted"
 
     def action_in_process(self):
-        self.state = "I_P"
+        for x in self:
+            x.state = "In process"
 
     def action_processed(self):
-        self.state = "P"
+        for x in self:
+            self.state = "Processed"
 
     def submitted_cron(self):
         element = self.env['kzm.instance.request'].search([('limit_date', '<=', date.today() + timedelta(days=5))])
         for x in element:
-            x.state = 'S'
+            x.action_submitted()
