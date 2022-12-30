899cd96d93f4c5277ce2962f351b5b1380377e70
MC-Karizma <c.mamady@karizma.ma>
2022-12-30 09:27:58 +0100

    [UPD] Modifications des accès aux boutons et menus

--- addons/kzm_instance_request/__manifest__.py
+++ addons/kzm_instance_request/__manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/kzm_instance_request_views.xml',
         'views/odoo_version_views.xml',
